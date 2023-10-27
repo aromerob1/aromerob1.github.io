1696351aa411f2b22cee75db98a41892636e9dcc
andrew01811 <52345691+andrew01811@users.noreply.github.com>
2023-10-26 19:48:32 -0500

    added 2 projects

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -12,22 +12,40 @@ function Project({ image, title, url, technologies }) {
       style={{ width: "18rem" }}
       onClick={handleClick}
     >
-      <img src={image} className="card-img-top" alt="project" />
+      <img src={image} className="card-img-top project-image" alt="project" />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        {technologies.map((technology, index) => {
-          let margin = "";
-          if (index !== 0) {
-            margin = "ms-3";
-          }
-          if (technology === "React") {
-            return <i class={`devicon-react-original technology ${margin}`} />;
-          } else if (technology === "Tailwind CSS") {
-            return <i class={`devicon-tailwindcss-plain technology ${margin}`} />;
-          } else if (technology === "Typescript") {
-            return <i className={`devicon-typescript-plain technology ${margin}`} />;
-          }
-        })}
+        <div className="title-container">
+          <h5 className="card-title">{title}</h5>
+        </div>
+        <div className="mt-3">
+          {technologies.map((technology, index) => {
+            let margin = "";
+            if (index !== 0) {
+              margin = "ms-3";
+            }
+            if (technology === "React") {
+              return (
+                <i className={`devicon-react-original technology ${margin}`} />
+              );
+            } else if (technology === "Tailwind CSS") {
+              return (
+                <i
+                  className={`devicon-tailwindcss-plain technology ${margin}`}
+                />
+              );
+            } else if (technology === "Typescript") {
+              return (
+                <i
+                  className={`devicon-typescript-plain technology ${margin}`}
+                />
+              );
+            } else if (technology === "Java") {
+              return (
+                <i className={`devicon-java-plain technology ${margin}`} />
+              );
+            }
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,7 @@
 import Project from "./Project";
 import Pi1 from "./assets/pg1.png";
+import Pi2 from "./assets/pg2.png";
+import Pi3 from "./assets/pg3.png";
 import "./Projects.css";
 
 function Projects() {
@@ -10,18 +12,34 @@ function Projects() {
       url: "https://aromerob1.github.io/gaming-marketplace/",
       technologies: "React;Tailwind CSS;Typescript",
     },
+    {
+      title: "Virtual Memory Management Simulation",
+      image: Pi2,
+      url: "https://github.com/aromerob1/virtual-memory-management-simulation",
+      technologies: "Java",
+    },
+    {
+      title: "Multi-Thread Factory Flow",
+      image: Pi3,
+      url: "https://github.com/aromerob1/MultiThreadFactoryFlow/tree/main",
+      technologies: "Java",
+    },
   ];
   return (
     <section id="projects" className="text-white mt-5">
       <h1 className="mb-4">Projects</h1>
-      {projects.map((project) => (
-        <Project
-          image={project.image}
-          title={project.title}
-          url={project.url}
-          technologies={project.technologies.split(";")}
-        />
-      ))}
+      <div className="row">
+        {projects.map((project) => (
+          <div className="col-3">
+            <Project
+              image={project.image}
+              title={project.title}
+              url={project.url}
+              technologies={project.technologies.split(";")}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
